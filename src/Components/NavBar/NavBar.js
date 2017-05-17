49e851156b3f242cd67246d1aeadcf8a695d0655
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-05-17 16:52:03 -0600

    added sticky nav bar effect

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+// import ReactDOM from 'react-dom'
 import './NavBar.scss'
 
 const pageName = 'Khayyam.ga'
@@ -22,25 +23,52 @@ const links = [
   }
 ]
 
-const NavBar = () => {
-  return (
-    <nav className='navbar navbar-default navbar-fixed-top'>
-      <div>
-        <a className='navbar-header' href='#page-top'>{pageName}</a>
-      </div>
-      <div>
-        <ul className='navbar-right'>
-          {links.map(link => {
-            return (
-              <li key={link.name}>
-                <a className='navbar-item' href={link.href}>{link.name}</a>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    </nav>
-  )
+let scrollTop
+
+class NavBar extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      transform: -60
+    }
+    this.handleScroll = this.handleScroll.bind(this)
+  }
+  componentDidMount () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll (event) {
+    scrollTop = event.srcElement.body.scrollTop
+    let itemTranslate = Math.min(0, scrollTop / 3 - 65)
+    this.setState({ transform: itemTranslate })
+  }
+
+  render () {
+    return (
+      <nav
+        className={this.state.transform === 0 ? 'navbar navbar-fixed-top' : 'navbar navbar-default'}
+        ref='navbar'>
+        <div>
+          <a className='navbar-header' href='#page-top'>{pageName}</a>
+        </div>
+        <div>
+          <ul className='navbar-right'>
+            {links.map(link => {
+              return (
+                <li key={link.name}>
+                  <a className='navbar-item' href={link.href}>{link.name}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </nav>
+    )
+  }
 }
 
 export default NavBar
