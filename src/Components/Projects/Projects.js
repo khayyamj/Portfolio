6eac75412a22fd736634701b30909d209be422c8
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-05-26 10:08:10 -0600

    added another project to project section, split out data for cleaner code

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,51 +1,7 @@
 import React from 'react'
 // import troop51Image from '../../images/portfolio/Troop51.png'
 import './Projects.scss'
-
-const projects = [
-  {
-    title: 'Troop 51',
-    skills: 'React - Node - SQL',
-    id: 'troop51',
-    projProps: '',
-    link: 'http://troop51.com'
-  },
-  {
-    title: 'Top Secret',
-    skills: 'Angular - Node - MongoDB',
-    id: 'topsecret',
-    projProps: '',
-    link: 'http://wearetopsecret.cf'
-  },
-  {
-    title: 'Troop 51',
-    skills: 'Other Skills Used',
-    id: 'project3',
-    projProps: '',
-    link: ''
-  },
-  {
-    title: 'Troop 51',
-    skills: 'Other Skills Used',
-    id: 'project4',
-    projProps: '',
-    link: ''
-  },
-  {
-    title: 'Troop 51',
-    skills: 'Other Skills Used',
-    id: 'project5',
-    projProps: '',
-    link: ''
-  },
-  {
-    title: 'Troop 51',
-    skills: 'Other Skills Used',
-    id: 'project6',
-    projProps: '',
-    link: ''
-  }
-]
+import projects from './project_list'
 
 const Projects = () => {
   return (
---
+++ src/Components/Projects/project_list.js
@@ -0,0 +1,46 @@
+const projects = [
+  {
+    title: 'Troop 51',
+    skills: 'React - Node - SQL',
+    id: 'troop51',
+    projProps: '',
+    link: 'http://troop51.com'
+  },
+  {
+    title: 'Top Secret',
+    skills: 'Angular - Node - MongoDB',
+    id: 'topsecret',
+    projProps: '',
+    link: 'http://wearetopsecret.cf'
+  },
+  {
+    title: 'Code Wars',
+    skills: 'JavaScript',
+    id: 'codewars',
+    projProps: '',
+    link: 'https://www.codewars.com/users/khayyamj'
+  },
+  {
+    title: 'Troop 51',
+    skills: 'Other Skills Used',
+    id: 'project4',
+    projProps: '',
+    link: ''
+  },
+  {
+    title: 'Troop 51',
+    skills: 'Other Skills Used',
+    id: 'project5',
+    projProps: '',
+    link: ''
+  },
+  {
+    title: 'Troop 51',
+    skills: 'Other Skills Used',
+    id: 'project6',
+    projProps: '',
+    link: ''
+  }
+]
+
+export default projects
