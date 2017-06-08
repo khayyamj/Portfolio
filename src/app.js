4c98744437375cb74ab6b76d85475e1dda445ac3
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-06-08 12:33:59 -0600

    removed useless code

--- src/app.js
+++ src/app.js
@@ -1,26 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Switch,
-//   Redirect
-// } from 'react-router-dom'
+
 import Routes from './Containers/Routes'
-// import Main from './Containers/Main/Main'
-// import Ancestry from './Containers/Ancestry/Main'
-// import My404 from './Containers/My404/My404'
 import './app.scss'
 
 ReactDOM.render(
-  // <Router>
-  //   <Switch>
-  //     <Route path='/' exact component={Main} />
-  //     <Route path='/ancestry' component={Ancestry} />
-  //     <Route component={My404} />
-  //     <Redirect from='*' to='/lost' />
-  //   </Switch>
-  // </Router>,
   <Routes />,
   document.getElementById('root')
   )
